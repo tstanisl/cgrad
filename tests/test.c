74e0d9362dbf0b149b0b901204963653a2327870
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2022-05-22 22:45:40 +0200

    test: add print()

--- tests/test.c
+++ tests/test.c
@@ -3,17 +3,21 @@
 #include <cgrad/matrix.h>
 #include <stdio.h>
 
-int main(void) {
-	cmat m = cmat_from_raw(2,3,(float[]){1,2,3,4,5,6}).get;
-	cmat m2 = cmat_add(m, cmat_add(m, cmat_scalar(2))).get;
-
-	CMAT_TO_VLA(m2, arr);
-	for (size_t r = 0; r < m2.rows; ++r) {
-		for (size_t c = 0; c < m2.cols; ++c) {
+void print(cmat m) {
+	CMAT_TO_VLA(m, arr);
+	for (size_t r = 0; r < m.rows; ++r) {
+		for (size_t c = 0; c < m.cols; ++c) {
 			printf("\t%f", arr[r][c]);
 		}
 		puts("");
 	}
+}
+
+int main(void) {
+	cmat m = cmat_from_raw(2,3,(float[]){1,2,3,4,5,6}).get;
+	cmat m2 = cmat_add(m, cmat_add(m, cmat_scalar(2))).get;
+
+	print(m2);
 
 	cmat_del(&m);
 	cmat_del(&m2);
